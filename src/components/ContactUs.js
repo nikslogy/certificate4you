f6a7d962a9f0db04ee9dd962d94c82de7f6624ad
nikslogy <nikitpotdar@gmail.com>
2024-09-17 13:49:21 +0530

    contact form chages 2

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -6,37 +6,40 @@ function ContactUs() {
     <div className="contact-us">
       <h1>Contact Us</h1>
       <p>For any inquiries, support, or bug reports, please email us at <a href="mailto:nikitpotdar@gmail.com">nikitpotdar@gmail.com</a> or fill out the form below:</p>
-      <form name="contact" action="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea
-            id="message"
-            name="message"
-            required
-          ></textarea>
-        </div>
-        <div data-netlify-recaptcha="true"></div>
-        <button type="submit">Send Message</button>
-      </form>
+      <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+  <input type="hidden" name="form-name" value="contact" />
+  <div hidden>
+    <input name="bot-field" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="name">Name</label>
+    <input
+      type="text"
+      id="name"
+      name="name"
+      required
+    />
+  </div>
+  <div className="form-group">
+    <label htmlFor="email">Email</label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      required
+    />
+  </div>
+  <div className="form-group">
+    <label htmlFor="message">Message</label>
+    <textarea
+      id="message"
+      name="message"
+      required
+    ></textarea>
+  </div>
+  <div data-netlify-recaptcha="true"></div>
+  <button type="submit">Send Message</button>
+</form>
     </div>
   );
 }
